1c1eb91ebdcddd8f0700e794ac6867988604bc1d
Kevin Chen <kevin.chen.krc@gmail.com>
2018-12-01 14:02:33 -0500

    Add database functions

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import os
+import sqlite3
 
 from flask import Flask, flash, jsonify, redirect, render_template, request, session, g
 from flask_session import Session
-import sqlite3
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
 
 from helpers import apology, search
+
 
 # Configure application
 app = Flask(__name__)
@@ -30,7 +31,6 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-
 # Set up database
 DATABASE = 'places.db'
 
@@ -38,7 +38,19 @@ def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
+
+    def make_dicts(cursor, row):
+        return dict((cursor.description[idx][0], value)
+                    for idx, value in enumerate(row))
+        
+    db.row_factory = make_dicts
     return db
+
+def query_db(query, args=()):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return rv
 
 @app.teardown_appcontext
 def close_connection(exception):
@@ -46,20 +58,23 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
+
 # Routes
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-	if request.method=="GET":
-		return render_template('index.html');
-	else:
-		return search(request.form.get('searchAddress'));
+    if request.method=="GET":
+        Places = query_db('select * from places')
+        return render_template('index.html', Places=Places);
+
+    else:
+        return search(request.form.get('searchAddress'));
 
 @app.route("/report", methods=["GET"])
 def report():
-	userType = request.args.get('userType')
-	return render_template('report.html')
-	
+    userType = request.args.get('userType')
+    return render_template('report.html')
+    
 
 def errorhandler(e):
     """Handle error"""
