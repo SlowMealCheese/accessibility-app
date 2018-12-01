617a2163d11a5fd4c6ea951ae6fc24b832dd23e4
Eryk Pecyna <eryk.pecyna@gmail.com>
2018-12-01 11:27:30 -0500

    OOf

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
 
-from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
@@ -26,10 +25,6 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
-
-# Custom filter
-app.jinja_env.filters["usd"] = usd
-
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
@@ -37,80 +32,19 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
-
+db = 
 
 @app.route("/")
-@login_required
 def index():
-    
-    return apology("TODO")
-
+    return render_template('index.html');
 
 @app.route("/buy", methods=["GET", "POST"])
-@login_required
 def buy():
     return apology("TODO")
-
-
-@app.route("/check", methods=["GET"])
-def check():
-    return apology("TODO")
-
-
-@app.route("/history")
-@login_required
-def history():
-    return apology("TODO")
-
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    return apology("TODO")
-
-
-@app.route("/logout")
-def logout():
-    return apology("TODO")
-
-
-@app.route("/quote", methods=["GET", "POST"])
-@login_required
-def quote():
-    if request.method == "POST":
-        # Get stock info and handle error if invalid symbol
-        iexQuote = lookup(request.form.get('symbol').strip())
-        if not iexQuote:
-            return apology('Please provide valid stock symbol')
-            # Build string to send to template with stock info
-        newQuote = "A share of " + iexQuote["name"] + " (" + iexQuote["symbol"] + ") costs $" + str(iexQuote["price"])
-        return render_template('quoted.html', quote=newQuote)
-    else:
-        return render_template('quote.html')
-    return apology("TODO")
-
-
-@app.route("/changepassword", methods=["GET", "POST"])
-@login_required
-def changepassword():
-    return apology("TODO")
-
-
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    return apology("TODO")
-
-
-@app.route("/sell", methods=["GET", "POST"])
-@login_required
-def sell():
-    return apology("TODO")
-
 
 def errorhandler(e):
     """Handle error"""
     return apology(e.name, e.code)
-
 
 # listen for errors
 for code in default_exceptions:
