c1deb358d97e7cff28daaaf679a11eef09c48f1b
Troy Appel <troyappel@college.harvard.edu>
2018-12-01 21:45:35 -0500

    Finished landing page from report

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ def report():
         query_db('INSERT INTO places (place_id, wheelchair, bathroom_access, door_width, table_height) VALUES (?, ?, ?, ?, ?)', 
                  (request.form.get("place_id"), request.form.get("wheelchair"), request.form.get("bathroom_access"),
                   request.form.get("door_width"), request.form.get("table_height")))
-        return redirect("/", success_message="Thank you! Your report was successfully logged!")
+        return render_template("index.html", success_message="Thank you! Your report was successfully logged!")
 
 @app.route("/nextpage")
 
